751a5e93f7fd1ba0b332bd30bce6f431b960b84a
Clayton Carlson <claytonrc97@gmail.com>
2023-07-28 15:29:14 -0700

    looks like twitter kinda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import {BiHomeCircle, BiUser} from 'react-icons/bi'
-import {BsBell, BsBookmark, BsTwitter} from 'react-icons/bs'
+import {BsBell, BsBookmark, BsEnvelope, BsTwitter} from 'react-icons/bs'
 import {HiOutlineHashtag} from 'react-icons/hi'
 import {HiEnvelope} from 'react-icons/hi2'
 import Link from 'next/Link';
 
 const NAVIGATION_ITEMS = [
+  {
+    title:'Twitter',
+    icon:BsTwitter
+
+  },
   {
     title: 'Home',
     icon:BiHomeCircle
@@ -19,7 +24,7 @@ const NAVIGATION_ITEMS = [
   },
   {
     title:'Messages',
-    icon:HiEnvelope
+    icon:BsEnvelope
   },
   {
     title:'Bookmarks',
@@ -37,26 +42,26 @@ const NAVIGATION_ITEMS = [
 const Home = () => {
   return (
     <div className='w-full h-full flex justify-center items-center relative bg-black'>
-      <div className='max-w-screen-lg w-full h-full flex relative'>
+      <div className='max-w-screen-xl w-full h-full flex relative'>
         {/* left sidebar for navigation/header */}
-        <section className='fixed w-72 flex flex-col h-screen'>
-          <Link href={'/'}>
-            <BsTwitter />
-          </Link>
-            {
-              NAVIGATION_ITEMS.map((item)=>(
-                <Link className='bg-white/50 flex items-center justify-center space-x-2 rounded-3xl p-4'href={`/${item.title.toLowerCase()}`} key={item.title}>
+        <section className='fixed w-[275px] flex flex-col items-stretch h-screen'>
+          <div className='flex flex-col items-stretch space-y-4 mt-4'>
+            {NAVIGATION_ITEMS.map((item)=>(
+                <Link className='hover:bg-white/10 text-3xl transition duration-200 flex items-center justify-start w-fit space-x-2 rounded-3xl py-6 px-6'href={`/${item.title.toLowerCase()}`} key={item.title}>
                   <div>
                       <item.icon/>
                   </div>
-                  <div>
-                    {
-                      item.title
-                    }
-                  </div>
+                  {item.title !== 'Twitter' && <div>{item.title}</div>}
+
                 </Link>
-              ))
-            }
+              ))}
+            <button className='rounded-full m-4 bg-primary p-4 text-2xl text-center hover:bg-opacity-70 transition duration-200'>
+              Tweet
+            </button>
+            </div>
+            <div>
+              at the bottom
+            </div>
         </section>
         <main>Home Timeline</main>
         <section>right section</section>
