775c52828671e41f664a1fdd9685f9fc544d1b5b
Clayton Carlson <claytonrc97@gmail.com>
2023-08-02 14:50:24 -0700

    stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import LeftSidebar from "./components/LeftSidebar"
 import MainComponent from "./components/MainComponent"
-
+import {BsSearch} from 'react-icons/bs'
 
 
 const Home = () => {
@@ -10,7 +10,18 @@ const Home = () => {
         {/* left sidebar for navigation/header */}
         <LeftSidebar />
         <MainComponent />
-        {/*<section>right section</section>*/}
+        <section className='fixed flex flex-col space-y-4'>
+        <div>
+          <div className='relative w-full h-full'>
+            <div className='absolute top-0 left-0 h-full flex items-center justify-center'>
+              <BsSearch className='w-5 h-5 text-gray-500'/>
+              </div>
+          <input type='text' placeholder='search' className='outline-none bg-transparent border-none w-full h-full rounded-xl py-4 px-6'/>
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+        </section>
       </div>
     </div>
   )
