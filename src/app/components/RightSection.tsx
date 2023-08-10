bda0945396f8bdd31d764df05f96ed3b8c125bf0
Clayton Carlson <claytonrc97@gmail.com>
2023-08-09 17:55:17 -0700

    search bar updated

--- src/app/components/RightSection.tsx
+++ src/app/components/RightSection.tsx
@@ -3,14 +3,18 @@ import { BsSearch } from 'react-icons/bs'
 
 const RightSection = () => {
   return (
-    <section className='w-full sticky hidden top-2 overflow-y-auto xl:flex flex-col items-stretch h-[90vh] overflow-x-hidden px-6'>
+    <section className='w-full sticky hidden top-2 mt-2 overflow-y-auto xl:flex flex-col items-stretch h-[90vh] overflow-x-hidden px-6'>
         <div>
           <div className='relative w-full h-full group'>
-            <label htmlFor='searchBox' className='absolute top-0 left-0 h-full flex items-center justify-center'>
-              <BsSearch className='w-5 h-5 text-gray-500'/>
+            <label htmlFor='searchBox' className='absolute top-0 left-0 h-full flex items-center justify-center p-4'>
+              <BsSearch className='w-5 h-5 text-gray-500 peer-focus:text-primary'/>
               </label>
-          <input id='searchBox' type='text' placeholder='search' className='outline-none bg-transparent border-none w-full h-full rounded-xl py-4 px-6'/>
-          </div>
+          <input 
+          id='searchBox' 
+          type='text' 
+          placeholder='search' 
+          className='outline-none peer focus:border-primary focus:border-2 bg-neutral-900/90 w-full h-full rounded-xl py-4 px-6'/>
+          </div> 
         </div>
         <div></div>
         <div></div>
